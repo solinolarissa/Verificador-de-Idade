ae6cc49165f1788b70b82e3b78c3adbcd0004b5d
solinolarissa <66344093+solinolarissa@users.noreply.github.com>
2020-06-26 01:41:58 -0300

    Textando o programa
    Textar se tudo está funcionando

--- script.js
+++ script.js
@@ -10,12 +10,42 @@ function Verificar(){
         var idade = ano - Number(fano.value)
         res.innerHTML = `Idade Calculada: ${idade}`
         var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked){
             gênero = 'Homem'
+            if (idade >=0 && idade < 10){
+                //Criança
+                img.setAttribute('src', 'criançamenino.png')
+            }else if (idade < 21){
+                //Jovem
+                img.setAttribute('src', 'jovemmenino.png')
+            }else if(idade < 50){
+                // Adulto
+                img.setAttribute('src', 'adulto.png')
+            }else {
+                //Idoso
+                img.setAttribute('src', 'idoso.png')
+            }
         }else if (fsex[1].checked) {
             gênero = 'Mulher'
+            if (idade >=0 && idade < 10){
+                //Criança
+                img.setAttribute('src', 'criançamenina.png')
+            }else if (idade < 21){
+                //Jovem
+                img.setAttribute('src', 'jovemmenina.png')
+            }else if(idade < 50){
+                // Adulto
+                img.setAttribute('src', 'adulta.png')
+            }else {
+                //Idoso
+                img.setAttribute('src', 'idosa.png')
+
+            }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${gênero} com ${idade} ano(s).`
+        res.appendChild(img)
     }
 }
