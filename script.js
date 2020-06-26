a03355a84ed922c2fd618d93f1e60944fc62e93c
solinolarissa <66344093+solinolarissa@users.noreply.github.com>
2020-06-26 01:15:44 -0300

    textando o script

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function Verificar(){
- 
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.legth == 0 || Number(fano.value) > ano){
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        res.innerHTML = `Idade Calculada: ${idade}`
+
+    }
 }
